39936bdb5162e1a19c395be9312c57adfee31f3d
hnmwt <69408037+hnmwt@users.noreply.github.com>
2022-01-03 19:35:14 +0900

    aaa

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@
 // // letは再宣言不可能
 // let val2 = "let変数を再宣言";
 
-const val3 = "const変数";
-console.log(val3);
+// const val3 = "const変数";
+// console.log(val3);
 
 // const変数は上書き不可
 //val3 = "const変数を上書き";
@@ -45,15 +45,101 @@ console.log(val3);
 /**
  *  テンプレート文字列
  */
-const name = "じゃけえ";
-const age = 28;
+// const name = "じゃけえ";
+// const age = 28;
 
 // 私の名前はじゃけえです。年齢は28歳です。
 
 //従来の方法
-const message1 = "私に名前は" + name + "です。年齢は" + age + "です。";
-console.log(message1);
+// const message1 = "私に名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/*
+ *　アロー関数
+ */
+
+//従来の関数
+//  function func1(str){
+//    return str;
+//  }
+//  const func1 = function(str){
+//    return str;
+//  }
+//  console.log(func1("func1です。"));
+
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です。"));
+
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 29));
+
+/**
+ * 分割代入
+ */
+// const myprofile = {
+//   name :"じゃけえ",
+//   age:28
+// };
+
+// const message1 = `名前は${myprofile.name}です。年齢は${myprofile.age}歳です。`;
+// console.log(message1);
+
+// const {name, age} = myprofile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myprofile = ['じゃけえ', 28];
+// const message3 = `名前は${myprofile[0]}です。年齢は${myprofile[1]}歳です。`
+// console.log(message3);
+
+// const [name, age] = myprofile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん`);
+// sayHello("じゃけえ");
+
+/**
+ * スプレッド構文
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+// 配列のコピーや結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+const arr6 = [...arr4];
+console.log(arr6);
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
